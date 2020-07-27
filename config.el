f65530443a0531b142eb3973602cee5141669c9c
huihao <2503019778@qq.com>
2020-07-27 15:43:20 +0800

    update user info

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "huihao"
+      user-mail-address "2503019778@qq.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
